5e9796368301dab0246382fda3eae2132afeaf5b
jordy1199 <cajasjordy11@gmail.com>
2026-07-24 02:20:44 -0500

    Mejoras de rendimiento

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,37 +1,41 @@
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import MainLayout from "../layout/MainLayout";
-import GalleryPage from "../pages/GalleryPage";
-import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/LoginPage";
-import ReservationsPage from "../pages/ReservationsPage";
-import ServicesPage from "../pages/ServicesPage";
-import StatusPage from "../pages/StatusPage";
-import TypesPage from "../pages/TypesPage";
 import "../pages/pages.css";
 import ProtectedRoute from "./ProtectedRoute";
 
+const HomePage = lazy(() => import("../pages/HomePage"));
+const ServicesPage = lazy(() => import("../pages/ServicesPage"));
+const GalleryPage = lazy(() => import("../pages/GalleryPage"));
+const TypesPage = lazy(() => import("../pages/TypesPage"));
+const StatusPage = lazy(() => import("../pages/StatusPage"));
+const ReservationsPage = lazy(() => import("../pages/ReservationsPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/servicios" element={<ServicesPage />} />
-        <Route path="/galeria" element={<GalleryPage />} />
-        <Route path="/tipos" element={<TypesPage />} />
-        <Route path="/estado" element={<StatusPage />} />
-        <Route
-          path="/reservaciones"
-          element={
-            <ProtectedRoute>
-              <ReservationsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/servicios" element={<ServicesPage />} />
+          <Route path="/galeria" element={<GalleryPage />} />
+          <Route path="/tipos" element={<TypesPage />} />
+          <Route path="/estado" element={<StatusPage />} />
+          <Route
+            path="/reservaciones"
+            element={
+              <ProtectedRoute>
+                <ReservationsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
